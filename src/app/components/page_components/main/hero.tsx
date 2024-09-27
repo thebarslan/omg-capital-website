b2c23d3db2eeef737a191ddce8d8c6488fab708c
Baran Arslan <69631182+thebarslan@users.noreply.github.com>
2024-09-27 13:25:46 +0300

    .

--- src/app/components/page_components/main/hero.tsx
+++ src/app/components/page_components/main/hero.tsx
@@ -9,25 +9,30 @@ const Hero = () => {
             <div className="absolute left-0 top-0 bg-[#000000be] w-full h-full z-[20] "></div>
 
             <div className="hero-bigger-container h-full w-full">
-               <Container>
-                  <div className="hero-container w-full flex relative z-[200] lg:h-[650px] md:h-[450px] h-[280px] text-white items-center">
-                     <div className="left flex flex-col">
-                        <div className="title-container lg:w-3/4 w-3/4 lg:mt-0 mt-5">
-                           <h5 className="lg:text-5xl text-xl font-bold">
-                              Guiding Your Investments Towards Success
-                           </h5>
-                        </div>
-                        <div className="button-container lg:mt-12 mt-8">
-                           <a
-                              href="/team"
-                              className="lg:px-10 lg:py-[10px] px-6 py-[6px] lg:text-lg text-[15px] bg-logoRed rounded-lg text-white border-2 border-logoRed hover:border-white hover:bg-white hover:text-black hover:bg-transparent transition-colors duration-300"
-                           >
-                              Check our team
-                           </a>
-                        </div>
+               <div className="hero-container w-full flex relative z-[200] lg:h-[650px] md:h-[450px] h-[280px] text-white items-center">
+                  <div className="left flex flex-col w-full items-center">
+                     <div className="title-container lg:full w-3/4 lg:mt-0 mt-5 items-center text-center">
+                        <h5 className="lg:text-5xl text-xl font-bold">
+                           Financial Strength, Secure Growth
+                        </h5>
+                     </div>
+                     <div className="line w-[200px] h-[2px] bg-white lg:mt-10 mt-4 lg:mb-8 mb-2"></div>
+                     <div className="title-container lg:full w-11/12 lg:mt-0 mt-5 items-center text-center">
+                        <h5 className="lg:text-lg text-base">
+                           By protecting your financial strength, we build
+                           sustainable growth together.
+                        </h5>
+                     </div>
+                     <div className="button-container lg:mt-16 mt-8">
+                        <a
+                           href="/team"
+                           className="lg:px-10 lg:py-[10px] px-6 py-[6px] lg:text-lg text-[15px] bg-logoRed rounded-lg text-white border-2 border-logoRed hover:border-white hover:bg-white hover:text-black hover:bg-transparent transition-colors duration-300"
+                        >
+                           Check our team
+                        </a>
                      </div>
                   </div>
-               </Container>
+               </div>
             </div>
          </div>
       </div>
